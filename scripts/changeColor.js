d4fc3c1ff6cf15fd3f184884bc4ff95fb0aa0c1c
habib-676 <rahman.habibur2421k@gmail.com>
2025-03-02 05:07:48 +0600

    change color script changed

--- scripts/changeColor.js
+++ scripts/changeColor.js
@@ -1,10 +1,9 @@
 function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 document.getElementById("theme-btn").addEventListener("click", function () {
