8914d770e01e31c5f67de408831af918945b4a90
habib-676 <rahman.habibur2421k@gmail.com>
2025-03-01 10:10:43 +0600

    all card scripts added

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,27 @@ document.getElementById("todays-date").innerText =
   todaysDate.toLocaleDateString();
 
 // ----------------------------------------------------
+
+// tasks
+
+const buttons = document.getElementsByClassName("card-btn");
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+    alert("Board Updated Successfully");
+    button.disabled = true;
+    let activeTask = parseInt(document.getElementById("active-task").innerText);
+    document.getElementById("active-task").innerText = activeTask - 1;
+
+    let completedTask = parseInt(
+      document.getElementById("completed-task").innerText
+    );
+    document.getElementById("completed-task").innerText = completedTask + 1;
+
+    // update activity
+
+    // last alert
+    if (activeTask - 1 === 0) {
+      alert("Congrats!! You have completed all the current tasks!");
+    }
+  });
+}
